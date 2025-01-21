66b2cd8e8a706b7472bcf6ceae14d9a1d40ca158
tau_mbp2018 <36781951+tndd@users.noreply.github.com>
2025-01-21 23:11:33 +0900

    実装箇所でテストの地産地消

--- src/sample/insight/count.py
+++ src/sample/insight/count.py
@@ -5,6 +5,10 @@ def count_data():
     data = collect_data_a()
     return len(data)
 
+# Test
+def test_count_data():
+    assert count_data() == 3
+
 
 if __name__ == "__main__":
     print(count_data())
